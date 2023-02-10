b077f057c5c2a09572bc15bd21da1e5791bc9fa2
MolluskARK <molluskark@gmail.com>
2023-02-10 12:39:09 -0700

    Improve Cryopods stat display hook

--- src/DinoStatsDisplay.cpp
+++ src/DinoStatsDisplay.cpp
@@ -3,17 +3,37 @@
 struct FFrame
 {
     UFunction*& NodeField() { return *GetNativePointerField<UFunction**>(this, "FFrame.Node"); }
+    UObject*& ObjectField() { return *GetNativePointerField<UObject**>(this, "FFrame.Object"); }
+    uint8*& CodeField() { return *GetNativePointerField<uint8**>(this, "FFrame.Code"); }
+    FFrame*& PreviousFrameField() { return *GetNativePointerField<FFrame**>(this, "FFrame.PreviousFrame"); }
 };
 
 UFunction* CryoUbergraphFunc;
+UFunction* CryoIngestFunc;
 
-// ArkServerApi hook used to add dino stat points to the cryopod widget
+// Read a uint64 from a BP script
+uint64 ReadQWORD(unsigned char* Script)
+{
+    int scriptIndex = 0;
+    uint64 Value = Script[scriptIndex++];
+    Value = Value | ((uint64)Script[scriptIndex++] << 8);
+    Value = Value | ((uint64)Script[scriptIndex++] << 16);
+    Value = Value | ((uint64)Script[scriptIndex++] << 24);
+    Value = Value | ((uint64)Script[scriptIndex++] << 32);
+    Value = Value | ((uint64)Script[scriptIndex++] << 40);
+    Value = Value | ((uint64)Script[scriptIndex++] << 48);
+    Value = Value | ((uint64)Script[scriptIndex] << 56);
+
+    return Value;
+}
+
+// ArkServerApi hook used to add dino stat points to the cryopod widget when a dino is captured
 DECLARE_HOOK(UPrimalItem_execSetCustomItemData, void, UPrimalItem*, FFrame*, void* const);
 void Hook_UPrimalItem_execSetCustomItemData(UPrimalItem* _this, FFrame* Stack, void* const Result)
 {
     AShooterWeapon* cryo;
     UProperty* dinoProp;
-    UProperty* dataProp;
+    UProperty* dataProp = nullptr;
     APrimalDinoCharacter** dino;
     FCustomItemData* data;
     char* naturalStats;
@@ -21,45 +41,58 @@ void Hook_UPrimalItem_execSetCustomItemData(UPrimalItem* _this, FFrame* Stack, v
     std::string newName;
 
     // Check if this function is being called while capturing a dino
-    if (Stack->NodeField() == CryoUbergraphFunc) {
-        cryo = _this->AssociatedWeaponField().Get();
-        if (cryo == nullptr) {
-            Log::GetLog()->error("NULL: AssociatedWeapon");
-        } else {
-            // Get the UProperties that point to the dino and the FCustomItemData being set
-            dinoProp = cryo->FindProperty(FName("CapturingDino", EFindName::FNAME_Find));
-            dataProp = cryo->FindProperty(FName("K2Node_MakeStruct_CustomItemData", EFindName::FNAME_Find));
-            if ((dinoProp == nullptr) || (dinoProp->ElementSizeField() != sizeof(APrimalDinoCharacter*))) {
-                Log::GetLog()->error("UProperty not found: CapturingDino");
-            } else if ((dataProp == nullptr) || (dataProp->ElementSizeField() != sizeof(FCustomItemData))) {
-                Log::GetLog()->error("UProperty not found: K2Node_MakeStruct_CustomItemData");
-            } else {
-                dino = (APrimalDinoCharacter**)((uint8*)cryo + dinoProp->Offset_InternalField());
-                data = (FCustomItemData*)((uint8*)cryo + dataProp->Offset_InternalField());
-                naturalStats = (*dino)->GetCharacterStatusComponent()->NumberOfLevelUpPointsAppliedField()();
-                tamedStats = (*dino)->GetCharacterStatusComponent()->NumberOfLevelUpPointsAppliedTamedField()();
+    if ((Stack->NodeField() != CryoUbergraphFunc) &&
+        (Stack->PreviousFrameField() == nullptr) &&
+        (Stack->PreviousFrameField()->NodeField() != CryoIngestFunc))
+    {
+        UPrimalItem_execSetCustomItemData_original(_this, Stack, Result);
+        return;
+    }
 
-                // Append stat points to the dino's name string displayed by the cryopod
-                newName = data->CustomDataStrings[1].ToString();
-                newName += "\n";
-                newName += "\nHealth:      " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Health]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::Health] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Health]);
-                newName += "\nStamina:   " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Stamina]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::Stamina] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Stamina]);
-                newName += "\nOxygen:     " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Oxygen]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::Oxygen] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Oxygen]);
-                newName += "\nFood:          " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Food]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::Food] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Food]);
-                newName += "\nWeight:       " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Weight]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::Weight] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Weight]);
-                newName += "\nDamage:    " + std::to_string(naturalStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier]);
-                newName += "\nSpeed:         " + std::to_string(naturalStats[EPrimalCharacterStatusValue::SpeedMultiplier]);
-                newName += (tamedStats[EPrimalCharacterStatusValue::SpeedMultiplier] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::SpeedMultiplier]);
+    // Get the cryopod weapon
+    cryo = (AShooterWeapon*)Stack->PreviousFrameField()->ObjectField();
 
-                data->CustomDataStrings[1] = FString(newName);
-            }
-        }
+    // Get the UProperty for the APrimalDinoCharacter* instance variable that points to the dino being captured
+    dinoProp = cryo->FindProperty(FName("CapturingDino", EFindName::FNAME_Find));
+
+    // Get the UProperty for the FCustomItemData instance variable that describes the captured dino
+    if (*Stack->CodeField() == (uint8)EExprToken::EX_InstanceVariable)
+        dataProp = (UProperty*)ReadQWORD(Stack->CodeField() + 1);
+
+    if ((dinoProp == nullptr) || (dinoProp->ElementSizeField() != sizeof(APrimalDinoCharacter*)))
+    {
+        Log::GetLog()->error("UProperty not found: CapturingDino");
+    }
+    else if ((dataProp == nullptr) || (dataProp->ElementSizeField() != sizeof(FCustomItemData)))
+    {
+        Log::GetLog()->error("UProperty not found: InData");
+    }
+    else
+    {
+        dino = (APrimalDinoCharacter**)((uint8*)cryo + dinoProp->Offset_InternalField());
+        data = (FCustomItemData*)((uint8*)cryo + dataProp->Offset_InternalField());
+        naturalStats = (*dino)->MyCharacterStatusComponentField()->NumberOfLevelUpPointsAppliedField()();
+        tamedStats = (*dino)->MyCharacterStatusComponentField()->NumberOfLevelUpPointsAppliedTamedField()();
+
+        // Append stat points to the dino's name string displayed by the cryopod
+        newName = data->CustomDataStrings[1].ToString();
+        newName += "\n";
+        newName += "\nHealth:      " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Health]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::Health] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Health]);
+        newName += "\nStamina:   " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Stamina]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::Stamina] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Stamina]);
+        newName += "\nOxygen:     " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Oxygen]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::Oxygen] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Oxygen]);
+        newName += "\nFood:          " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Food]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::Food] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Food]);
+        newName += "\nWeight:       " + std::to_string(naturalStats[EPrimalCharacterStatusValue::Weight]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::Weight] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::Weight]);
+        newName += "\nDamage:    " + std::to_string(naturalStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::MeleeDamageMultiplier]);
+        newName += "\nSpeed:         " + std::to_string(naturalStats[EPrimalCharacterStatusValue::SpeedMultiplier]);
+        newName += (tamedStats[EPrimalCharacterStatusValue::SpeedMultiplier] == 0) ? "" : " +" + std::to_string(tamedStats[EPrimalCharacterStatusValue::SpeedMultiplier]);
+
+        data->CustomDataStrings[1] = FString(newName);
     }
 
     UPrimalItem_execSetCustomItemData_original(_this, Stack, Result);
@@ -68,6 +101,10 @@ void Hook_UPrimalItem_execSetCustomItemData(UPrimalItem* _this, FFrame* Stack, v
 // Called when the server is ready
 void Plugin_ServerReadyInit()
 {
+    CryoIngestFunc = (UFunction*)Globals::FindClass("Function /Game/Extinction/CoreBlueprints/Weapons/WeapEmptyCryopod.WeapEmptyCryopod_C:Ingest");
+    if (CryoIngestFunc == nullptr)
+        Log::GetLog()->error("UFunction not found: WeapEmptyCryopod_C::Ingest");
+
     CryoUbergraphFunc = (UFunction*)Globals::FindClass("Function /Game/Extinction/CoreBlueprints/Weapons/WeapEmptyCryopod.WeapEmptyCryopod_C:ExecuteUbergraph_WeapEmptyCryopod");
     if (CryoUbergraphFunc == nullptr)
         Log::GetLog()->error("UFunction not found: WeapEmptyCryopod_C::ExecuteUbergraph_WeapEmptyCryopod");
